649cfb85e7d19cc3290beab84b3ce5615d6705ea
guitarranalon <guitarranalon@gmail.com>
2024-07-19 23:28:15 +0200

    [TVS-5] converting the third level menu template to Astro

---
+++ src/helpers/get-third-level-menu.ts
@@ -0,0 +1,39 @@
+export function getThirdLevelMenu(tree: any, currentPage: string) {
+    for (let page of tree.pages) {
+      let result = getPageChildren(page, null, 3, currentPage);
+  
+      if (result !== null) {
+        return result;
+      }
+    }
+    return null;
+  }
+  
+  function getPageChildren(page: any, parentPage: any, level: number, currentPage: string): any {
+    let result = null;
+  
+    if (currentPage === page.path) {
+      if (level === 1) {
+        return parentPage.children;
+      }
+      if (page.children) {
+        return page.children;
+      } else {
+        return null;
+      }
+    } else {
+      if (page.children) {
+        for (let childPage of page.children) {
+          result = getPageChildren(childPage, page, level - 1, currentPage);
+  
+          if (result !== null) {
+            return result;
+          }
+        }
+        return null;
+      } else {
+        return null;
+      }
+    }
+  }
+  
