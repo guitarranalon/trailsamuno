8bb03b47d91588fbcdcc235c3c39db579148d28f
guitarranalon <guitarranalon@gmail.com>
2024-07-24 15:44:33 +0200

    [TVS-7] trying to solve deploy problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ export default defineConfig({
     build: {
         format: "file",
         site: 'https://guitarranalon.github.io',
-        base: '/trailsamuno',
+        base: 'trailsamuno',
     },    
 });
