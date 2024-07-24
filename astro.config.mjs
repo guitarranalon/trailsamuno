af8f56d93d6914b01c8b47cc00d198189bfb2ec1
guitarranalon <guitarranalon@gmail.com>
2024-07-24 15:39:01 +0200

    [TVS-7] new deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     build: {
-        format: "file"
+        format: "file",
+        site: 'https://guitarranalon.github.io',
+        base: '/trailsamuno',
     },    
 });
