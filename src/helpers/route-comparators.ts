e6700ca5e7fd6998fdff7d256088ddbd1e3960db
guitarranalon <guitarranalon@users.noreply.github.com>
2024-07-25 23:17:53 +0200

    [manageGHPagesRoutes] fixing path bugs because of the base url in gh pages (#2)
    * [TVS-8] managing resources url's
    
    * [TVS-8] function refactor
    
    * [TVS-8] multiple font choice
    
    * [TVS-8] solving problem with font import
    
    * [TVS-8] fixing path bugs
    
    ---------
    
    Co-authored-by: robertocg <cancioroberto@uniovi.es>

---
+++ src/helpers/route-comparators.ts
@@ -0,0 +1,23 @@
+function addTrailingSlashIfNotExist(route: string): string {
+    return route.endsWith("/") ? route : `${route}/`;
+}
+
+function prefixWithBaseUrlIfNecessary(route: string): string {
+    if (route.startsWith(import.meta.env.BASE_URL)) return route;
+
+    return `${import.meta.env.BASE_URL}${route}`;
+}
+
+function normalizeRoute(route: string): string {
+    let normalizedRoute = prefixWithBaseUrlIfNecessary(route);
+    normalizedRoute = addTrailingSlashIfNotExist(normalizedRoute);
+    return normalizedRoute;
+}
+
+export function isSameRoute(route1: string, route2: string): boolean {
+    return normalizeRoute(route1) === normalizeRoute(route2);
+}
+
+export function routeIsBaseUrl(route: string): boolean {
+    return isSameRoute(route, import.meta.env.BASE_URL);
+}
