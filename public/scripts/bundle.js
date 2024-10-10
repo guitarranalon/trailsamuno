aa3d7f6ffda9ea911a7b67f7553229058eb0a73b
guitarranalon <guitarranalon@users.noreply.github.com>
2024-10-11 00:40:29 +0200

    [TVS-13]Race Cards (#3)
    * [TVS-13] starting with race cards
    
    * [TVS-13] removing old race data blocks and animations
    
    * [TVS-13] starting with the styles of the race card
    
    * [TVS-13] improving styles
    
    * [TVS-13] improving race card format
    
    * [TVS-13] improving race card styles
    
    * [TVS-13] improving card design
    
    * [TVS-13] styles for the date
    
    * [TVS-13] finishing race cards
    
    ---------
    
    Co-authored-by: robertocg <cancioroberto@uniovi.es>

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -12579,16 +12579,12 @@ window.progressBarsAnimations = function(selector){
 };
 
 window.indexAnimations = function(){
-	var $datosTvs = $('.data').eq(0),
-		$datosTvxs = $('.data').eq(1),
-		positionDatosTvs = $datosTvs.offset(),
-		positionDatosTvxs = $datosTvxs.offset(),
-		$excerpts = $('.excerpt');
+	var $excerpts = $('.excerpt');
 		
 window.prepareIndexAnimations = function(){
 	var $content = $('#content-block'),
 		$elements = $content
-						.find('.data > li, .excerpt:not(:first-of-type)>.row>h2, .excerpt>.row>p, .excerpt>.row>.button, .excerpt>.row>img');
+						.find('.excerpt:not(:first-of-type)>.row>h2, .excerpt>.row>p, .excerpt>.row>.button, .excerpt>.row>img');
 	
 	$elements.addClass('transparent');
 };
@@ -12596,42 +12592,6 @@ window.prepareIndexAnimations = function(){
 	$(function domReady($) {
 			prepareIndexAnimations();
 		
-			$datosTvs.scrollspy({
-				min: positionDatosTvs.top - ($(window).height()/2),
-				max: positionDatosTvs.top + $(this).height(),
-				onEnter: function onEnter(element) {
-					window.console.log('Entering ' + element.id);
-					
-					$datosTvs.find('>li').each(function (i) {
-						$(this).delay(i*100).queue(function (next) {
-							$(this).removeClass('transparent').addClass('animated bounceIn');
-							next(); // just to dequeue
-						});
-					});
-					
-					
-					
-					
-					
-					
-				}
-			});
-			
-			$datosTvxs.scrollspy({
-				min: positionDatosTvxs.top - ($(window).height()/2),
-				max: positionDatosTvxs.top + $(this).height(),
-				onEnter: function onEnter(element) {
-					window.console.log('Entering ' + element.id);
-					
-					$datosTvxs.find('>li').each(function (i) {
-						$(this).delay(($datosTvxs.find('>li').length - i)*100).queue(function (next) {
-							$(this).removeClass('transparent').addClass('animated bounceIn');
-							next(); // just to dequeue
-						});
-					});					
-				}
-			});
-
 			for(var i=1, max = $excerpts.length; i<max; i++){
 				var $el = $excerpts.eq(i);
 				$el.scrollspy({
