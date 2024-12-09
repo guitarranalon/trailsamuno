137e6aa0632b837d99a1980f1a9a58be5a38f4af
guitarranalon <guitarranalon@gmail.com>
2024-12-09 13:14:44 +0100

    [TVS-41] refactor to encapsulate logic

--- src/scripts/desarrollo/cookie-preferences.js
+++ src/scripts/desarrollo/cookie-preferences.js
@@ -1,80 +1,101 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const banner = document.getElementById("cookie-banner");
-  const preferencesDiv = document.getElementById("cookie-preferences");
+  const cookieManager = new CookiePreferences();
+  cookieManager.init();
+});
 
-  const acceptAllButton = document.getElementById("accept-all");
-  const rejectAllButton = document.getElementById("reject-all");
-  const configureButton = document.getElementById("configure");
-  const savePreferencesButton = document.getElementById("save-preferences");
-
-  const analyticsCheckbox = document.getElementById("analytics");
-  const externalcontentCheckbox = document.getElementById("externalcontent");
-
-  configureButton?.addEventListener("click", () => {
-    if (!preferencesDiv) return;
-
-    preferencesDiv.style.display = "block";
-  });
-
-  acceptAllButton?.addEventListener("click", () => {
-    savePreferences({ analytics: true, externalcontent: true });
-    hideBanner();
-  });
-
-  rejectAllButton?.addEventListener("click", () => {
-    savePreferences({ analytics: false, externalcontent: false });
-    hideBanner();
-  });
-
-  savePreferencesButton?.addEventListener("click", () => {
-    const preferences = {
-      analytics: analyticsCheckbox?.checked,
-      externalcontent: externalcontentCheckbox?.checked,
+class CookiePreferences {
+  constructor() {
+    this.htmlElements = {
+      banner: document.getElementById("cookie-banner"),
+      preferencesDiv: document.getElementById("cookie-preferences"),
+      acceptAllButton: document.getElementById("accept-all"),
+      rejectAllButton: document.getElementById("reject-all"),
+      configureButton: document.getElementById("configure"),
+      savePreferencesButton: document.getElementById("save-preferences"),
+      analyticsCheckbox: document.getElementById("analytics"),
+      externalcontentCheckbox: document.getElementById("externalcontent"),
     };
-    savePreferences(preferences);
-    hideBanner();
-  });
-
-  function savePreferences(preferences) {
-    localStorage.setItem("cookiePreferences", JSON.stringify(preferences));
   }
 
-  function hideBanner() {
-    if (!banner) return;
+  init() {
+    this.#addEventListeners();
 
-    banner.style.display = "none";
-    testCookiePreferences();
+    const savedPreferences = (new CookiePreferences).getCookiePreferences();
+    if (savedPreferences) {
+      this.#hideBanner();
+    }    
+  }
+
+  #addEventListeners() {
+    this.htmlElements.configureButton?.addEventListener("click", () => {
+      if (!this.htmlElements.preferencesDiv) return;
+  
+      this.htmlElements.preferencesDiv.style.display = "block";
+    });
+  
+    this.htmlElements.acceptAllButton?.addEventListener("click", () => {
+      this.#savePreferences({ analytics: true, externalcontent: true });
+      this.#hideBanner();
+    });
+  
+    this.htmlElements.rejectAllButton?.addEventListener("click", () => {
+      this.#savePreferences({ analytics: false, externalcontent: false });
+      this.#hideBanner();
+    });
+  
+    this.htmlElements.savePreferencesButton?.addEventListener("click", () => {
+      const preferences = {
+        analytics: this.htmlElements.analyticsCheckbox?.checked,
+        externalcontent: this.htmlElements.externalcontentCheckbox?.checked,
+      };
+      this.#savePreferences(preferences);
+      this.#hideBanner();
+    });
+  }  
+
+  getCookiePreferences() {
+    const preferences = localStorage.getItem("cookiePreferences");
+    return preferences ? JSON.parse(preferences) : null;
+  }
+
+  #loadExternalContent() {
+    const externalIframes = document.querySelectorAll("iframe[data-src]");
+    externalIframes.forEach((iframe) => {
+      const iframeSrc = iframe.getAttribute("data-src");
+
+      if (iframeSrc !== null) {
+        iframe.setAttribute("src", iframeSrc);
+      }
+    });
+  }
+
+  #loadAnalytics() {
     runTagManager();
   }
 
-  const savedPreferences = localStorage.getItem("cookiePreferences");
-  if (savedPreferences) {
-    hideBanner();
+  #savePreferences(preferences) {
+    localStorage.setItem("cookiePreferences", JSON.stringify(preferences));
   }
-});
 
-function getCookiePreferences() {
-  const preferences = localStorage.getItem("cookiePreferences");
-  return preferences ? JSON.parse(preferences) : null;
-}
+  #hideBanner() {
+    if (!this.htmlElements?.banner) return;
+  
+    this.htmlElements.banner.style.display = "none";
+    new CookiePreferences().applyPreferences();
+    runTagManager();
+  }  
 
-function loadExternalContent() {
-  const externalIframes = document.querySelectorAll("iframe[data-src]");
-  externalIframes.forEach((iframe) => {
-    const iframeSrc = iframe.getAttribute("data-src");
+  applyPreferences() {
+    const cookiePreferences = this.getCookiePreferences();
 
-    if (iframeSrc !== null) {
-      iframe.setAttribute("src", iframeSrc);
+    if (cookiePreferences?.externalcontent) {
+      this.#loadExternalContent();
     }
-  });
-}
 
-function testCookiePreferences() {
-  const cookiePreferences = getCookiePreferences();
-
-  if (cookiePreferences?.externalcontent) {
-    loadExternalContent();
+    if (cookiePreferences?.analytics) {
+      this.#loadAnalytics();
+    }
   }
 }
 
-window.testCookiePreferences = testCookiePreferences;
+window.CookiePreferences = CookiePreferences;
