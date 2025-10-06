b4ca7cb3cc4f4ea20448dd28dc16d48a54050b8f
guitarranalon <guitarranalon@users.noreply.github.com>
2025-10-06 11:20:17 +0200

    Backend rankings (#22)
    * [TVS-79] generating palmareses and historic ranking when building instead of hardcode it in an astro file and calculate on the users navigator
    
    * [TVS-79] removing client script for the historic ranking plus finishing the historic ranking on the build

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -12433,7 +12433,7 @@ window.progressBarsAnimations = function(selector){
 				min: position.top - ($(window).height()/2),
 				max: position.top + $(this).height(),
 				onEnter: function onEnter(element) {
-					window.console.log('Entering ' + element.id);
+					// window.console.log('Entering ' + element.id);
 					
 					for(var i=0, max = $progressBars.length; i<max; i++){
 						$progressBars.eq(i).attr('style', 'width: ' + $progressBars.eq(i).attr('aria-valuenow') + '%');
@@ -12526,94 +12526,6 @@ $.scrollUp({scrollText:"Volver arriba"});
 $("#content").find(".gallery").featherlightGallery({galleryFadeIn:300,openSpeed:300,type:"image"});
 
 initScrollHeader();
-
-class Ranking {
-    #results = [];
-    #mainBlock = document.querySelector("#content-block");
-
-    #generateTable(data) {
-        const tabla = document.createElement('table');
-        const thead = document.createElement('thead');
-        const tbody = document.createElement('tbody');
-        let referenceAthlete = data[0];
-        let athleteIndex = 1;
-
-        const encabezado = document.createElement('tr');
-        encabezado.appendChild(document.createElement('th')).textContent = '';
-        encabezado.appendChild(document.createElement('th')).textContent = 'Atleta';
-        encabezado.appendChild(document.createElement('th')).textContent = 'Oro';
-        encabezado.appendChild(document.createElement('th')).textContent = 'Plata';
-        encabezado.appendChild(document.createElement('th')).textContent = 'Bronce';
-
-        thead.appendChild(encabezado);
-
-        data.forEach((athlete, index) => {
-            const fila = document.createElement('tr');
-            if (this.#sortData(athlete, referenceAthlete) !== 0) {
-                referenceAthlete = athlete;
-                athleteIndex = index + 1;
-            }
-            fila.appendChild(document.createElement('td')).textContent = athleteIndex;
-
-            for (const clave in athlete) {
-                const celda = document.createElement('td');
-                celda.textContent = athlete[clave];
-                fila.appendChild(celda);
-            }
-            tbody.appendChild(fila);
-        });
-
-        tabla.appendChild(thead);
-        tabla.appendChild(tbody);
-
-        this.#mainBlock.appendChild(tabla);
-    }
-
-    #addResult(athlete, pos) {
-        if (athlete !== '' && athlete !== '-') {
-            let athleteResult = this.#results.find((res) => res.name === athlete);
-
-            if (!athleteResult) {
-                athleteResult = { 
-                    name: athlete, 
-                    gold: pos === 1 ? 1 : 0,
-                    silver: pos === 2 ? 1 : 0,
-                    bronze: pos === 3 ? 1 : 0,
-                };
-
-                this.#results.push(athleteResult);
-            } else {
-                athleteResult.gold += pos === 1 ? 1 : 0;
-                athleteResult.silver += pos === 2 ? 1 : 0;
-                athleteResult.bronze += pos === 3 ? 1 : 0;
-            }
-        }
-    }
-
-    #getData() {
-        document.querySelectorAll("table tr").forEach((row) => {
-            row.querySelectorAll("td").forEach((el, index) => {
-                const athlete = el.innerHTML.trim();
-                this.#addResult(athlete, index + 1);
-            });
-        });
-    }
-
-    #sortData(a, b) {
-        if (b.gold !== a.gold) return b.gold - a.gold;
-        if (b.silver !== a.silver) return b.silver - a.silver;
-        return b.bronze - a.bronze;
-    }
-
-    generateRanking() {
-        this.#getData();
-        this.#results = this.#results.sort(this.#sortData);
-        this.#mainBlock.appendChild(document.createElement('h3')).textContent = 'Ranking histórico';
-        this.#generateTable(this.#results);
-    }
-}
-
-window.Ranking = Ranking;
 
 document.addEventListener("DOMContentLoaded", () => {
   const cookieManager = new CookiePreferences();
